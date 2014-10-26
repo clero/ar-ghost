3161f57689a9c14fc4962aff7a11ba881280cda8
clero <clero@etud.insa-toulouse.fr>
2014-10-26 10:57:23 +0000

    [LibDroneMovement] Add AtCommandFactory Class for the libDroneMovement
    Our project needs an embedded version of the ARDrone Middleware
    in order to let the drone be able to move by himself.
    The SDK seems too complex to this job.
    
    The AtCommandFactory class is the first part of this Middleware.
    It allows to create drone's AtCommands and handle the sequence
    number autonomously.
    
    It's also the first part of the libDroneMovement which will be
    embedded in the drone.
    
    Signed-off-by: clero <clero@etud.insa-toulouse.fr>

---
+++ libDroneMovement/src/AtCommandFactory.cpp
@@ -0,0 +1,237 @@
+/*
+ * Copyright 2014 Jules Cléro
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include "libDroneMovement/AtCommandFactory.hpp"
+
+#include <cstdint>
+
+namespace ghost
+{
+namespace libDroneMovement
+{
+
+AtCommandFactory::AtCommandFactory() : mSequence(1)
+{
+}
+
+const std::string AtCommandFactory::mAtCommandPrefix = "AT*";
+const std::string AtCommandFactory::mAtCommandAssignement = "=";
+const std::string AtCommandFactory::mAtCommandSuffix = "\r";
+const std::string AtCommandFactory::mAtCommandSeparator = ",";
+
+const std::string AtCommandFactory::mAtCommandRefHeader = "REF";
+const std::string AtCommandFactory::mAtCommandPcmdHeader = "PCMD";
+const std::string AtCommandFactory::mAtCommandPcmdMagHeader = "PCMD_MAG";
+const std::string AtCommandFactory::mAtCommandFtrimHeader = "FTRIM";
+const std::string AtCommandFactory::mAtCommandConfigHeader = "CONFIG";
+const std::string AtCommandFactory::mAtCommandComWdgHeader = "COMWDG";
+const std::string AtCommandFactory::mAtCommandCalibHeader = "CALIB";
+
+const std::string AtCommandFactory::commandFactory(const std::string& atCmdHeader)
+{
+    // Command's stream initialisation
+    std::ostringstream atCmd;
+    atCmd << mAtCommandPrefix << atCmdHeader << mAtCommandAssignement << mSequence
+          << mAtCommandSuffix;
+
+    incrSequenceNumber();
+
+    // Get the final AT command
+    return atCmd.str();
+}
+
+void AtCommandFactory::setBit(int32_t& bitField, int bitToSet)
+{
+    bitField |= 1 << bitToSet;
+}
+
+void AtCommandFactory::clearBit(int32_t& bitField, int bitToClear)
+{
+    bitField &= ~(1 << bitToClear);
+}
+
+int AtCommandFactory::floatingPointConversion(float argToConv)
+{
+    return *(reinterpret_cast<int*>(&argToConv));
+}
+
+void AtCommandFactory::incrSequenceNumber()
+{
+    // if the sequence number had overflowed, we reset it to one
+    mSequence == 0 ? 1 : mSequence++;
+}
+
+int32_t AtCommandFactory::prepareRefHeaderFlag()
+{
+    int32_t flag = 0;
+
+    // When sending AT*REF command, bits 18, 20, 22, 24 and 28 needs to be set
+    setBit(flag, 18);
+    setBit(flag, 20);
+    setBit(flag, 22);
+    setBit(flag, 24);
+    setBit(flag, 28);
+
+    return flag;
+}
+
+const std::string AtCommandFactory::takeOffCommand()
+{
+    int32_t cmd = prepareRefHeaderFlag();
+    setBit(cmd, 9);
+
+    return commandFactory<int>(mAtCommandRefHeader, cmd);
+}
+
+const std::string AtCommandFactory::landCommand()
+{
+    int32_t cmd = prepareRefHeaderFlag();
+
+    return commandFactory<int>(mAtCommandRefHeader, cmd);
+}
+
+const std::string AtCommandFactory::emergencyCommand()
+{
+    int32_t cmd = prepareRefHeaderFlag();
+    setBit(cmd, 8);
+
+    return commandFactory<int>(mAtCommandRefHeader, cmd);
+}
+
+const std::string AtCommandFactory::hoveringCommand()
+{
+    /* Hovering mode is activated if the bit 0 of the flag arguments from
+     * a PCMD atCommand is set to 0
+     */
+    int32_t flag = 0;
+
+    return commandFactory<int>(mAtCommandPcmdHeader, flag, 0, 0, 0, 0);
+}
+
+const std::string AtCommandFactory::movementCommand(float roll, float pitch, float gaz, float yaw)
+{
+    int32_t flag = 0;
+
+    // Setting flag bit 0 to avoid entering in hovering mode
+    setBit(flag, 0);
+
+    return commandFactory<int>(
+        mAtCommandPcmdHeader,
+        flag,
+        floatingPointConversion(roll),
+        floatingPointConversion(pitch),
+        floatingPointConversion(gaz),
+        floatingPointConversion(yaw));
+}
+
+const std::string AtCommandFactory::movementCommand(float roll, float pitch, float gaz)
+{
+    int32_t flag = 0;
+
+    // Setting  bit 0 to avoid entering in hovering mode
+    setBit(flag, 0);
+    // Setting flag bit 1 to let the firmware choose yaw parameter in function of the roll one
+    setBit(flag, 1);
+
+    return commandFactory<int>(
+        mAtCommandPcmdHeader,
+        flag,
+        floatingPointConversion(roll),
+        floatingPointConversion(pitch),
+        floatingPointConversion(gaz),
+        0);
+}
+
+const std::string AtCommandFactory::movementWithMagnetoCommand(
+    float roll,
+    float pitch,
+    float gaz,
+    float yaw,
+    float magnetoPsi,
+    float magnetoPsiAccuracy)
+{
+    int32_t flag = 0;
+
+    // Setting flag bit 0 to avoid entering in hovering mode
+    setBit(flag, 0);
+
+    return commandFactory<int>(
+        mAtCommandPcmdMagHeader,
+        flag,
+        floatingPointConversion(roll),
+        floatingPointConversion(pitch),
+        floatingPointConversion(gaz),
+        floatingPointConversion(yaw),
+        floatingPointConversion(magnetoPsi),
+        floatingPointConversion(magnetoPsiAccuracy));
+}
+
+const std::string AtCommandFactory::movementWithMagnetoCommand(
+    float roll,
+    float pitch,
+    float gaz,
+    float magnetoPsi,
+    float magnetoPsiAccuracy)
+{
+    int32_t flag = 0;
+
+    // Setting flag bit 0 to avoid entering in hovering mode
+    setBit(flag, 0);
+
+    // Setting flag bit 1 to let the firmware choose yaw parameter in function of the roll one
+    setBit(flag, 1);
+
+    return commandFactory<int>(
+        mAtCommandPcmdMagHeader,
+        flag,
+        floatingPointConversion(roll),
+        floatingPointConversion(pitch),
+        floatingPointConversion(gaz),
+        0,
+        floatingPointConversion(magnetoPsi),
+        floatingPointConversion(magnetoPsiAccuracy));
+}
+
+const std::string AtCommandFactory::resetWatchdogCommand()
+{
+    return commandFactory(mAtCommandComWdgHeader);
+}
+
+const std::string AtCommandFactory::horizontalPlaneCalibrationCommand()
+{
+    return commandFactory(mAtCommandFtrimHeader);
+}
+
+const std::string AtCommandFactory::configureCommand(const std::string& argA,
+                                                     const std::string& argB)
+{
+    return commandFactory<std::string>(
+        mAtCommandConfigHeader,
+        "\"" + argA + "\"",
+        "\"" + argB + "\"");
+}
+
+const std::string AtCommandFactory::magnetoCalibrationCommand()
+{
+    // The magnetometer is the only device which can be calibrated
+    int deviceToCalibrate = 0;
+
+    return commandFactory<int>(mAtCommandCalibHeader, deviceToCalibrate);
+}
+
+} /* ghost namespace */
+} /* libDroneMovement namespace  */
